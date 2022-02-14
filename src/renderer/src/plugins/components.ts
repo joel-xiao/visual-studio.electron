0654116501136bbb7a1bcc74a9fbb4bd35d86bd2
肖文龙 <joel.xiao@dclingcloud.com>
2022-02-14 16:19:06 +0800

    eslint

--- src/renderer/src/plugins/components.ts
+++ src/renderer/src/plugins/components.ts
@@ -3,7 +3,7 @@ import { App } from 'vue';
 // const uses = require.context('../components', true, /use.ts$/);
 const uses = import.meta.globEager('../components/**/*.ts');
 export default {
-  install(app): void {
+  install(app: App<Element>): void {
     for (const path of Object.keys(uses)) {
       const install = uses[path].default?.install;
       install && install(app);
