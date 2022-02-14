8c045b59853d2785b09d2e04631ee48f32f70874
肖文龙 <joel.xiao@dclingcloud.com>
2022-02-14 16:02:05 +0800

    eslint

--- src/renderer/jest.config.js
+++ src/renderer/jest.config.js
@@ -1,4 +1,7 @@
+import path from 'path';
 module.exports = {
+  verbose: true,
+  rootDir: path.join(__dirname, '.'),
   moduleFileExtensions: ['vue', 'js', 'ts'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
@@ -7,5 +10,10 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest' // ts 文件用 ts-jest 转换
   },
   // 匹配 __tests__ 目录下的 .js/.ts 文件 或其他目录下的 xx.test.js/ts xx.spec.js/ts
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts)$'
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts)$',
+  moduleDirectories: ['node_modules']
+  // moduleNameMapper: {
+  //   '@/(.*)$': '<rootDir>/src/$1',
+  //   '@v/(.*)$': '<rootDir>/src/views/$1'
+  // }
 };
--- src/renderer/tests/Test.spec.ts
+++ src/renderer/tests/Test.spec.ts
@@ -1,8 +1,8 @@
-import { mount } from '@vue/test-utils';
-import Test from '@/views/Test.vue';
+import { shallowMount } from '@vue/test-utils';
+import Test from '../src/views/Test.vue';
 
 test('Test.vue', async () => {
-  const wrapper = mount(Test);
+  const wrapper = shallowMount(Test);
   expect(wrapper.html()).toContain('Unit Test Page');
   expect(wrapper.html()).toContain('count is: 0');
   await wrapper.find('button').trigger('click');
